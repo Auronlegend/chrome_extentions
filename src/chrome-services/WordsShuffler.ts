500e44ac2ee7a95d955c8cc9187c4e4fef72dd99
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2023-08-30 16:31:24 +0200

    feat: fix permission error, shuffle words

--- src/chrome-services/WordsShuffler.ts
+++ src/chrome-services/WordsShuffler.ts
@@ -1,23 +1,24 @@
 import { getTextNodes } from '../utils/Utils';
 
 const utilShuffleArray = (array: any[]): any[] => {
-  let newArr: any[] = []
-  for (let i = array.length - 1; i > 0; i--) {
+  array.forEach((_, i) => {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
-    newArr = [...newArr, array]
-  }
-  return newArr
-}
+  });
+  return array;
+};
 
 export const shuffleWords = (root: HTMLElement | Node): void => {
-  console.log(root, 'root')
+  console.log(root, 'root');
   getTextNodes(root).forEach((element) => {
-    console.log(element.textContent, 'content')
-    if (element.nodeType === Node.TEXT_NODE) {
-      if (element.textContent !== null) {
-        element.textContent = utilShuffleArray(element.textContent.split(' ')).join(' ');
-      }
+    if (element.nodeType === Node.TEXT_NODE && element.textContent !== null) {
+      element.textContent = utilShuffleArray(
+        element.textContent
+          .trim()
+          .split(' ')
+          .map((e) => e.replace(/(\r\n|\n|\r)/gm, ''))
+          .filter((e) => e.length !== 0 || !e.includes('\n'))
+      ).join(' ');
     }
-  })
-}
+  });
+};
