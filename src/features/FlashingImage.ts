9387f6d9f5d9131cbc3de8f4a548b1263afad9bb
PierpaoloPirasSp <pierpaolo.piras@spindox.it>
2023-09-13 14:58:24 +0200

    feat: somo stop foword to flosh omogos

--- src/features/FlashingImage.ts
+++ src/features/FlashingImage.ts
@@ -1,26 +1,41 @@
 import { getNodesByFilter } from '../utils/Utils';
 
 const localImages = [
-  'assets/1.jpeg',
-  'assets/2.jpeg',
-  'assets/3.jpeg',
-  'assets/4.jpeg',
-  'assets/5.jpeg',
-  'assets/6.jpeg',
-  'assets/7.jpeg',
-  'assets/8.jpeg',
-  'assets/9.jpeg',
-  'assets/10.jpeg'
+  '1.jpeg',
+  '2.jpeg',
+  '3.jpeg',
+  '4.jpeg',
+  '5.jpeg',
+  '6.jpeg',
+  '7.jpeg',
+  '8.jpeg',
+  '9.jpeg',
+  '10.jpeg'
 ];
 
 export const flashImagesInDocument = (root: HTMLElement | Node): void => {
-  console.log('🚀 ~ file: FlashingImage.ts:17 ~ flashImagesInDocument ~ root:', root)
   getNodesByFilter(root, 'SHOW_ALL').forEach((element) => {
-    if (element?.nodeName?.toUpperCase() === 'DIV') {
-      console.log('🚀 ~ file: FlashingImage.ts:18 ~ getNodesByFilter ~ element:', element.nodeName)
+    // if (element?.nodeName?.toUpperCase() === 'DIV') {
+    //   if (Math.random() < 0.1 && element.parentElement !== null) {
+    //     element.parentElement.style.backgroundImage = `url("${chrome.runtime.getURL(localImages[Math.floor(Math.random() * localImages.length)])}`
+    //     //  .background = `url("${chrome.runtime.getURL(
+    //     //       localImages[Math.floor(Math.random() * localImages.length)]
+    //     //     )}")`;
+    //     // document.body.style.backgroundSize = 'contain';
+    //   }
+    // }
+    if (element?.nodeName?.toUpperCase() === 'IMG') {
+      console.log('🚀 ~ file: FlashingImage.ts:29 ~ getNodesByFilter ~ element:', element)
       if (element.parentElement !== null) {
-        console.log('🚀 ~ file: FlashingImage.ts:22 ~ getNodesByFilter ~ element:', element)
-        element.parentElement.style.background = `url("${chrome.runtime.getURL(localImages[Math.floor(Math.random() * localImages.length)])}`
+        const a = element.parentElement
+        const b = a.querySelectorAll('img')
+        let newElement = null
+        b.forEach(el => {
+          newElement = document.createElement('img')
+          newElement.src = localImages[Math.floor(Math.random() * localImages.length)]
+          element.parentElement?.replaceChild(el, newElement)
+        })
+        // element .forEach(el =>  setAttribute('src', `url("${chrome.runtime.getURL(localImages[Math.floor(Math.random() * localImages.length)])}`))
         //  .background = `url("${chrome.runtime.getURL(
         //       localImages[Math.floor(Math.random() * localImages.length)]
         //     )}")`;
